65b902e24d294b508850e4e434d179a6dfa1ad64
Dylan Zhang <dylanzhang@Dylans-MacBook-Pro.local>
2023-03-15 22:37:52 -0700

    create a new avatar

---
+++ backend/controllers/actor.js
@@ -0,0 +1,26 @@
+const Actor = require('../models/actor');
+const cloudinary = require('cloudinary').v2;
+
+
+// Configuration 
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
+    secure: true
+});
+
+
+exports.createActor = async (req, res) => {
+    const { name, about, gender } = req.body;
+    const { file } = req;
+
+    const newActor = new Actor({ name, about, gender });
+
+    const { secure_url, public_id } = await cloudinary.uploader.upload(file.path);
+
+    newActor.avatar = { url: secure_url, public_id };
+    await newActor.save();
+    res.status(201).json(newActor);
+
+}
